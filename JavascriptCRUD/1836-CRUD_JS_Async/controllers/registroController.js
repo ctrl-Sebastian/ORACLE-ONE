a04d67071847aeadcf16aa211f227f3e49b937dc
Sebastian Marrera <smarrera@cincinnatus.edu.do>
2023-09-13 23:02:31 -0400

    Terminado el portafolio

---
+++ JavascriptCRUD/1836-CRUD_JS_Async/controllers/registroController.js
@@ -0,0 +1,17 @@
+import { clientService } from "../service/client-service.js";
+
+const formulario = document.querySelector("[data-form]")
+
+formulario.addEventListener("submit", (event)=>{
+  event.preventDefault();
+
+  const nombre = document.querySelector("[data-nombre]").value
+  const email = document.querySelector("[data-email]").value
+  
+  clientService
+  .crearCliente(nombre, email)
+  .then(() => {
+    window.location.href = "/screens/registro_completado.html"
+  })
+  .catch((error) => console.log(error));
+})
